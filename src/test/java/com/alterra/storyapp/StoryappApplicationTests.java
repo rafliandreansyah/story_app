a3d29188438c31065e1c27119f10af5b4cd54c3f
rafli <rafli@mediainovasi.id>
2022-10-24 09:50:46 +0700

    initial commit

---
+++ src/test/java/com/alterra/storyapp/StoryappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alterra.storyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
